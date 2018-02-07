4a39ce01eb0333f1d44505b145a7685b5b47b6fa
Haysel Santiago <hayselsantiago@zipcoders-MacBook-Pro-7.local>
2018-02-07 08:55:31 -0500

    Hi Bob, Hi Alice

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
 
+        Scanner nm = new Scanner(System.in);
+        System.out.println("Enter your first name: ");
+        String uname= nm.nextLine();
+
+        if(uname.equals("Bob"))
+        {
+            System.out.println("Hi Bob!");
+        }
+        else if (uname .equals("Alice"))
+        {
+            System.out.println("Hi ALice!");
+        }
+        else
+            System.out.println("You shall not pass!");
+        }
     }
-}
